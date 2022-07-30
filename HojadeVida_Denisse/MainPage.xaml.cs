8f7b2e6a1b2e59fede7b37e95abadbc5a3549e24
Noe19 <denissecumbal2015@hotmail.com>
2022-07-30 17:44:02 -0500

    Hoja de vida en Xamarin

---
+++ HojadeVida_Denisse/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HojadeVida_Denisse
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btn_Denisse(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+        private void btn_Ivan(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Ivan());
+        }
+        private void btn_Paul(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Paul());
+        }
+
+        private void btn_Noemi(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Noemi());
+        }
+
+    }
+}
